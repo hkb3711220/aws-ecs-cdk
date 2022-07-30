f2d19cd3f8997127001dff8d39ea85c3b51009b3
hkb3711220 <h3711220@gmail.com>
2022-07-30 13:31:17 +0900

    update ecs.py

--- modules/ecs.py
+++ modules/ecs.py
@@ -6,13 +6,14 @@ from aws_cdk import (
     aws_ecs_patterns as ecs_patterns,
 )
 from constructs import Construct
-
+from datetime import datetime
 class EcsStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
         common_name = "shanghaixiaochi"
+        time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         ecr_arn="arn:aws:ecr:ap-northeast-1:894724431332:repository/shanghaixiaochi"
         vpc = ec2.Vpc(self, "{}-vpc".format(common_name), max_azs=3)
         cluster = ecs.Cluster(self, "{}-cluster".format(common_name), vpc=vpc)
@@ -24,7 +25,8 @@ class EcsStack(Stack):
             desired_count=1,
             task_image_options=ecs_patterns.ApplicationLoadBalancedTaskImageOptions(
                 image=ecs.ContainerImage.from_ecr_repository(repository, tag="latest"),
-                container_port=3000
+                container_port=3000,
+                docker_labels={"release-date": time }
             ),
             memory_limit_mib=4096,
             public_load_balancer=True
